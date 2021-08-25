61e5e7d0fedfffabaa4b5b4afb6cbe73db9f7f84
Ahembenrizk <ahlem@ibsolutions.com.tn>
2021-08-25 14:55:29 +0100

    comit_Nature

---
+++ src/Presentation/Tsi.Template.Web/Factories/NatureModelFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tsi.Template.Core.Attributes;
+using Tsi.Template.ViewModels;
+
+namespace Tsi.Template.Web.Factories
+{
+    [Injectable(typeof(INatureModelFactory))]
+    public class NatureModelFactory : INatureModelFactory
+    {
+        public Task<NatureViewModel> PrepareNatureViewModelAsync() => Task.FromResult(new NatureViewModel());
+    }
+}
